d6270b89513389f8063b7bbc1a882d52294cc9a5
Elena Bychenkova <elena.bychenkova@gmail.com>
2022-04-28 08:55:56 -0700

    feat: implement pets cards popup window

--- shelter/scripts/burgerMenu.js
+++ shelter/scripts/burgerMenu.js
@@ -2,20 +2,20 @@ export const burgerMenu = () => {
 	const burgerBtn = document.getElementsByClassName("header__burger-btn")[0];
 	const navList = document.getElementsByClassName("header__nav-list")[0];
 	const navLinks = document.getElementsByClassName("nav-link");
-	const shadow = document.getElementsByClassName("shadow-box")[0];
+	const menuShadow = document.getElementsByClassName("menu-shadow")[0];
 	const header = document.getElementsByClassName("header")[0];
 	const body = document.getElementsByTagName("body")[0];
 
 	const toggleMenu = () => {
 		burgerBtn.classList.toggle("active");
 		navList.classList.toggle("active");
-		shadow.classList.toggle("active");
+		menuShadow.classList.toggle("active");
 		header.classList.toggle("active");
-		body.classList.toggle("active");
+		body.classList.toggle("scrollblock");
 	};
 
 	burgerBtn.addEventListener("click", toggleMenu);
-	shadow.addEventListener("click", toggleMenu);
+	menuShadow.addEventListener("click", toggleMenu);
 	for (let i = 0; i < navLinks.length; i++) {
 		navLinks[i].addEventListener("click", toggleMenu);
 	}
